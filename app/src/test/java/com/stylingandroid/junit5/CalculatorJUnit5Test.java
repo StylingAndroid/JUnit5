d335dc735c515b989af3a8bb391d1f66fbe99c68
Mark Allison <mark@stylingandroid.com>
2017-02-25 15:02:03 +0000

    Introduces DynamicTests to reduce boilerplate further

--- app/src/test/java/com/stylingandroid/junit5/CalculatorJUnit5Test.java
+++ app/src/test/java/com/stylingandroid/junit5/CalculatorJUnit5Test.java
@@ -1,110 +1,67 @@
 package com.stylingandroid.junit5;
 
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Locale;
+import java.util.Set;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.Executable;
+import org.junit.jupiter.api.TestFactory;
 
 import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 
 @SuppressWarnings("unused")
 public class CalculatorJUnit5Test {
 
     private Calculator calculator;
-    final float input1 = 1;
+    private final float input1 = 1;
 
     @BeforeEach
     void setup() {
         calculator = new Calculator();
     }
 
-    @Nested
+    @TestFactory
     @DisplayName("Given inputs of One and Two")
-    class oneAndTwo {
+    Collection<DynamicTest> oneAndTwo() {
         final float input2 = 2;
-
-        @Test
-        @DisplayName("When we add them Then the result is Three")
-        void plus() {
-            performTest(input1, input2, Operator.PLUS, 3f);
-        }
-
-        @Test
-        @DisplayName("When we subtract them Then the result is Minus One")
-        void minus() {
-            performTest(input1, input2, Operator.MINUS, -1f);
-        }
-
-        @Test
-        @DisplayName("When we multiply them Then the result is Two")
-        void multiply() {
-            performTest(input1, input2, Operator.MULTIPLY, 2f);
-        }
-
-        @Test
-        @DisplayName("When we divide them Then the result is Zero Point Five")
-        void divide() {
-            performTest(input1, input2, Operator.DIVIDE, 0.5f);
-        }
-
-        @Test
-        @DisplayName("When we modulo them Then the result is One")
-        void modulo() {
-            performTest(input1, input2, Operator.MODULO, 1f);
-        }
-
-        @Test
-        @DisplayName("When we apply an Unknown operator to them Then the result is Zero")
-        void unknown() {
-            performTest(input1, input2, Operator.UNKNOWN, 0f);
-        }
+        Set<DynamicTest> tests = new LinkedHashSet<>(6);
+        tests.add(createTest(input1, input2, Operator.PLUS, 3f));
+        tests.add(createTest(input1, input2, Operator.MINUS, -1f));
+        tests.add(createTest(input1, input2, Operator.MULTIPLY, 2f));
+        tests.add(createTest(input1, input2, Operator.DIVIDE, 0.5f));
+        tests.add(createTest(input1, input2, Operator.MODULO, 1f));
+        tests.add(createTest(input1, input2, Operator.UNKNOWN, 0f));
+        return tests;
     }
 
-    @Nested
+    @TestFactory
     @DisplayName("Given inputs of One and Zero")
-    class oneAndZero {
+    Collection<DynamicTest> oneAndZero() {
         final float input2 = 0;
-
-        @Test
-        @DisplayName("When we add them Then the result is One")
-        void plus() {
-            performTest(input1, input2, Operator.PLUS, 1f);
-        }
-
-        @Test
-        @DisplayName("When we subtract them Then the result is One")
-        void minus() {
-            performTest(input1, input2, Operator.MINUS, 1f);
-        }
-
-        @Test
-        @DisplayName("When we multiply them Then the result is Zero")
-        void multiply() {
-            performTest(input1, input2, Operator.MULTIPLY, 0f);
-        }
-
-        @Test
-        @DisplayName("When we divide them Then the result is Infinity")
-        void divide() {
-            performTest(input1, input2, Operator.DIVIDE, Float.POSITIVE_INFINITY);
-        }
-
-        @Test
-        @DisplayName("When we modulo them Then the result is NaN")
-        void modulo() {
-            performTest(input1, input2, Operator.MODULO, Float.NaN);
-        }
-
-        @Test
-        @DisplayName("When we apply an Unknown operator to them Then the result is Zero")
-        void unknown() {
-            performTest(input1, input2, Operator.UNKNOWN, 0f);
-        }
+        Set<DynamicTest> tests = new LinkedHashSet<>(6);
+        tests.add(createTest(input1, input2, Operator.PLUS, 1f));
+        tests.add(createTest(input1, input2, Operator.MINUS, 1f));
+        tests.add(createTest(input1, input2, Operator.MULTIPLY, 0f));
+        tests.add(createTest(input1, input2, Operator.DIVIDE, Float.POSITIVE_INFINITY));
+        tests.add(createTest(input1, input2, Operator.MODULO, Float.NaN));
+        tests.add(createTest(input1, input2, Operator.UNKNOWN, 0f));
+        return tests;
     }
 
-    void performTest(float value1, float value2, Operator operator, float expected) {
-        float result = calculator.calculate(value1, value2, operator);
+    private DynamicTest createTest(final float value1, final float value2, final Operator operator, final float expected) {
+        String displayName = String.format(Locale.getDefault(), "When we %s them Then the result is %.2f", operator.name(), expected);
+        return dynamicTest(displayName, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                float result = calculator.calculate(value1, value2, operator);
 
-        assertThat(result).isEqualTo(expected);
+                assertThat(result).isEqualTo(expected);
+            }
+        });
     }
 }
